3244e058efa31027177b617f211c84df8bfa1685
Justin Johnson <jj@MacBook-Pro.local>
2023-08-29 20:24:00 -0600

    Rename staging title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "JJ QA",
+  title: "JJ QA - STAGING",
   description: "Justin Johnson - QA",
 };
 
